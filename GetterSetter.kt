ed2aea693936f4e1a8be8036d5de6f3ac9f149e3
akungithub007 <96878297+akungithub007@users.noreply.github.com>
2021-12-30 23:38:01 +0800

    Add files via upload

---
+++ GetterSetter.kt
@@ -0,0 +1,11 @@
+package TugasPraktikum
+
+fun main() {
+    val note = Note("Yuk Bisa Yuk")
+
+    println(note.title)
+    note.title = ""
+}
+
+//println(note.title) diatas untuk memanggil get yang ada di class note yg sudah saya buat sebelumnya.
+//sedangkan note.title itu untuk memanggil setternya.
